c49f7da9116b9aa9626cd6ffe19ab0d74dba60b2
djanilson alves <djanilson.alves@gmail.com>
2019-10-08 21:05:55 -0300

    Add restart game
    - Add a button to restart game

--- pong.js
+++ pong.js
@@ -34,7 +34,7 @@ function draw() {
 		cx.fillRect(player[i].x, player[i].y, player[i].width, player[i].height);
 		cx.stroke();
 	}
-    
+
 	// Draw Ball
 	cx.fillRect(ball.x, ball.y, ball.width, ball.height);
 	cx.stroke();
@@ -59,15 +59,15 @@ function game() {
 	for (var i = 0; i < player.length; i++) {
 		if (
 			ball.y == player[i].y &&
-			ball.x >= player[i].x && 
+			ball.x >= player[i].x &&
 			ball.x <= (player[i].x + player[i].width)
 		) {
 			moveBallY *= -1;
 		}
 	}
-	
+
 	// Walls
-	if(ball.x >= width || ball.x <= 0) { 
+	if(ball.x >= width || ball.x <= 0) {
 		moveBallX *= -1;
 	}
 
@@ -91,12 +91,12 @@ function game() {
 		ball.x += moveBallX;
 		ball.y += moveBallY;
 	}
-	
+
 	// Move Player 2
 	if (!pause) {
 		if (ball.x < player[1].x) npcDirection = -1;
 		if (ball.x > player[1].x + player[1].width) npcDirection = 1;
-		
+
 		if (npcDirection < 0) {
 			if (player[1].x - playerSpeed >= 0) {
 				player[1].x -= playerSpeed * 0.75;
@@ -122,7 +122,7 @@ window.addEventListener("keydown", event => {
 	if (event.isComposing || event.keyCode === 229) {
 		return;
 	}
-	
+
 	if (event.keyCode == 65 || event.keyCode == 37) {
 		// Move Left
 		if (player[0].x - playerSpeed >= 0) {
@@ -146,15 +146,15 @@ function drawBanner() {
 		cx.lineWidth = 1;
 		cx.fillStyle = "black";
 		cx.fillRect(0, 0, width, height);
-		
+
 		// Banner Pos
 		banner--;
-		
+
 		// Draw Text
 		cx.fillStyle = "#FFF";
 		cx.font = "14px Courier";
 		cx.fillText("FAVIPONG!", banner, height-4);
-		
+
 		// Update Favicon
 		changeFavicon(createImage("image/png"));
 	}
@@ -171,4 +171,10 @@ function play() {
 	}
 }
 
-
+// restart game
+function restart() {
+	player[0] = { x: 6, y: 13, width: 4, height: 1, score: 0 };
+	player[1] = { x: 6, y: 1, width: 4, height: 1, score: 0 };
+	ball = { x: 7, y: 7, width: 1, height: 1 };
+	cx.fillRect(0, 0, width, height);
+}
